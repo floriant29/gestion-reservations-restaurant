1d0cfbd618d145c4b422c6e4b831ce24d357b028
floriant29 <63879034+floriant29@users.noreply.github.com>
2025-09-01 20:49:56 +0200

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,7 +6,8 @@ function App() {
   const msalConfig = {
     auth: {
       clientId: process.env.REACT_APP_AZURE_CLIENT_ID,
-      authority: \`https://login.microsoftonline.com/\${process.env.REACT_APP_AZURE_TENANT_ID}\`,
+      authority: `https://login.microsoftonline.com/${process.env.REACT_APP_AZURE_TENANT_ID}`,
+
       redirectUri: window.location.origin,
     },
   };
