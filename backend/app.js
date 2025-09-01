b88469366a789fb17bad1c891ddf8dfda17267d8
floriant29 <63879034+floriant29@users.noreply.github.com>
2025-09-01 20:44:52 +0200

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const axios = require('axios');
 const app = express();
 const port = process.env.PORT || 3000;
+const cors = require('cors');
 
 app.use(express.json());
 app.use(cors());
